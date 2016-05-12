b3223040876161851339caa3c2d9f6bccb1edf9a
Davimor <davimor39@gmail.com>
2016-05-12 09:43:49 +0200

    Add files via upload

---
+++ plantilla/bundle.js
@@ -0,0 +1,66 @@
+const {Observable} = Rx;
+const {div, p, img, span, h2, input, makeDOMDriver} = CycleDOM;
+
+function view(){
+  const footer = {'0':{'0':'Lalalala', '1': 'lala'}, '1':{'0': 'Lelelele', '1': 'lele'} };
+  return Observable.of(
+      div([
+        renderHeader(),
+        renderContent(),
+        renderFooter(footer),
+      ])
+    );
+}
+
+function renderHeader() {
+  return Observable.of(
+      div('.header', [
+       div('.container',[
+         div('.title-conainer .row',[
+           img('.logo',{src:'http://cycle.js.org/img/cyclejs_logo.svg', alt:'Cycle.js'}),
+           span('.title','Cycle.js'),
+         ]),
+         h2('A functional and reactive JavaScript framework for cleaner code'),
+       ]),
+      ])
+  );
+}
+
+function renderFooter(obj) {
+  return Observable.of(
+      div('.footer', [
+       div('.container',[
+          renderBox(obj)
+       ]),
+      ])
+  );
+}
+
+function renderBox(obj){
+   return Object.keys(obj).map( key => 
+      div('.col-lg-6 .col-md-6 .col-xs-12',[
+        renderParagraf(obj[key]),
+      ])
+    );
+}
+
+function renderContent(){
+  return div('.body',[]);
+}
+
+function renderParagraf(obj){
+  return Object.keys(obj).map( key => p(obj[key]));
+}
+
+function main(sources) {
+
+  const vtree$ = view();
+ 
+  return {
+    DOM: vtree$
+  };
+}
+
+Cycle.run(main, {
+  DOM: makeDOMDriver('#app')
+});
